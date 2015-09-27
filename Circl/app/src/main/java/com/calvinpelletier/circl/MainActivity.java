880fe0ba4b5e9753e9c630207fa45cf2676ec185
Wilson Chaney <wilsongchaney@gmail.com>
2015-09-27 01:39:16 -0500

    implemented fade in and out

--- Circl/app/src/main/java/com/calvinpelletier/circl/MainActivity.java
+++ Circl/app/src/main/java/com/calvinpelletier/circl/MainActivity.java
@@ -9,6 +9,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
@@ -27,45 +29,18 @@ public class MainActivity extends ActionBarActivity {
 
         pvContainer.addView(p);
 
+        final MainActivity m = this;
+
 
         View hamburger = findViewById(R.id.hamburgerMenu);
         hamburger.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                LayoutInflater mInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                final View view;
-                final View fadeView = createFadeView();
-                /* We inflate the xml which gives us a view */
-                view = mInflater.inflate(R.layout.overlay_menu,null, false);
-
-                mainLayout.addView(fadeView);
-                mainLayout.addView(view);
-
-                Button closeButton = (Button)view.findViewById(R.id.buttonX);
-                closeButton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        mainLayout.removeView(view);
-                        mainLayout.removeView(fadeView);
-                    }
-                });
-
-
-
+                OverlayFader.fadeIn(m,mainLayout);
             }
         });
     }
 
-    private View createFadeView()
-    {
-        View view = new View(this);
-        view.setBackgroundColor(Color.argb(196, 0, 0, 0));
-        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
-
-        view.setLayoutParams(params);
-
-        return view;
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
