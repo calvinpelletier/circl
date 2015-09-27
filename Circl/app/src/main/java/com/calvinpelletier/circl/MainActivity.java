6f508a179a296f0ab5dec450c9e83fe6c843cca0
Wilson Chaney <wilsongchaney@gmail.com>
2015-09-26 23:44:19 -0500

    begin dev on overlay menu

--- Circl/app/src/main/java/com/calvinpelletier/circl/MainActivity.java
+++ Circl/app/src/main/java/com/calvinpelletier/circl/MainActivity.java
@@ -1,9 +1,17 @@
 package com.calvinpelletier.circl;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +20,33 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final RelativeLayout mainLayout = (RelativeLayout)findViewById(R.id.mainLayout);
+
+        View hamburger = findViewById(R.id.hamburgerMenu);
+        hamburger.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LayoutInflater mInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                View view;
+                /* We inflate the xml which gives us a view */
+                view = mInflater.inflate(R.layout.overlay_menu,null, false);
+
+                // mainLayout.addView(createFadeView());
+                // mainLayout.addView(view);
+
+            }
+        });
+    }
+
+    private View createFadeView()
+    {
+        View view = new View(this);
+        view.setBackgroundColor(Color.argb(128,0,0,0));
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        params.setMargins(0,0,0,0);
+        view.setLayoutParams(params);
+        return view;
     }
 
     @Override
