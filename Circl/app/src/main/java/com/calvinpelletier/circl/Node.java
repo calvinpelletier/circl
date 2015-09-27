167546fdd2ac27af371746759d9800a5b4692bb0
Calvin Pelletier <calvin.pelletier@gmail.com>
2015-09-26 20:43:10 -0500

    Created Node and Coord class.

---
+++ Circl/app/src/main/java/com/calvinpelletier/circl/Node.java
@@ -0,0 +1,38 @@
+package com.calvinpelletier.circl;
+
+import android.graphics.Color;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Calvin Pelletier on 9/26/15.
+ */
+public class Node {
+
+    private ArrayList<Connection> connectionArray = new ArrayList<Connection>();
+    private int outlineColor = Color.BLACK;
+    private Coord position;
+    private final int radius = 50;
+
+    public Node(Coord position, int outlineColor) {
+        this.position = position;
+        this.outlineColor = outlineColor;
+    }
+
+    public Coord getPosition() {
+        return this.position;
+    }
+
+    public setPosition(Coord newPosition) {
+        this.position = newPosition;
+    }
+
+    public setPosition(int x, int y) {
+        this.position.x = x;
+        this.position.y = y;
+    }
+
+    public setOutline(int newColor) {
+        this.outlineColor = newColor;
+    }
+}
