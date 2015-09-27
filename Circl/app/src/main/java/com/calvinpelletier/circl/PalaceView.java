95298fe1847cfb08e3989f23128f50274479eabb
Wilson Chaney <wilsongchaney@gmail.com>
2015-09-26 21:50:48 -0500

    implemented some solid node drawing

--- Circl/app/src/main/java/com/calvinpelletier/circl/PalaceView.java
+++ Circl/app/src/main/java/com/calvinpelletier/circl/PalaceView.java
@@ -17,18 +17,12 @@ public class PalaceView extends View {
 
     PVGestureController gestureController;
 
-    private enum Mode
-    {
-        NONE,
-        PANNING
-    }
-
     public PalaceView(Context context)
     {
         super(context);
 
         mScaleDetector = new ScaleGestureDetector(context, new ScaleListener());
-        gestureController = new PVGestureController();
+        gestureController = new PVGestureController(400,400);
     }
 
     private Canvas canvas;
@@ -39,18 +33,85 @@ public class PalaceView extends View {
 
         canvas.save();
         canvas.scale(mScaleFactor, mScaleFactor);
-        canvas.translate(gestureController.getTranslateX(),gestureController.getTranslateY());
+        canvas.translate(gestureController.getTranslateX(), gestureController.getTranslateY());
 
         // Just drawing a blue circle for the time being, so I can test panning and zooming
-        Paint p = new Paint(Paint.ANTI_ALIAS_FLAG);
-        p.setColor(Color.BLUE);
-        canvas.drawCircle(50, 50, 200, p);
+        //Paint p = new Paint(Paint.ANTI_ALIAS_FLAG);
+        //p.setColor(Color.BLUE);
+        //canvas.drawCircle(50, 50, 200, p);
 
+        Node n1 = new Node(new Coord(100,100), Color.YELLOW);
+        Node n2 = new Node(new Coord(490,120), Color.GREEN);
+        Node n3 = new Node(new Coord(400,500), Color.MAGENTA);
+        Node n4 = new Node(new Coord(200,300), Color.RED);
+
+        drawNode(canvas,n1);
+        drawNode(canvas,n2);
+        drawNode(canvas,n3);
+        drawNode(canvas,n4);
+
+        drawConnection(canvas,n2,n3);
+        drawConnection(canvas,n1,n3);
+        drawConnection(canvas,n1,n4);
 
         canvas.restore();
     }
 
-    // http://stackoverflow.com/questions/25941097/android-pinch-zoom-and-panning-for-canvas-something-doesnt-work
+    // r should be somewhere in between 50 and 100 for now.
+    private void drawNode(Canvas canvas,Node n)
+    {
+        Paint nodePaint = new Paint();
+        nodePaint.setColor(Color.rgb(0,49,94));
+        canvas.drawCircle(n.getPosition().x, n.getPosition().y, n.getRadius(), nodePaint);
+
+
+        nodePaint = new Paint();
+        nodePaint.setStyle(Paint.Style.STROKE);
+        nodePaint.setStrokeWidth(5.0f);
+        nodePaint.setColor(n.getOutline());
+        canvas.drawCircle(n.getPosition().x, n.getPosition().y, n.getRadius(), nodePaint);
+    }
+
+    private void drawConnection(Canvas canvas,Node n1,Node n2)
+    {
+        int x1 = n1.getPosition().x;
+        int y1 = n1.getPosition().y;
+        int x2 = n2.getPosition().x;
+        int y2 = n2.getPosition().y;
+
+        int r1 = n1.getRadius();
+        int r2 = n2.getRadius();
+
+        int kx = 1;
+        int ky = 1;
+        if(x1 > x2)
+            kx = -1;
+        if(y1 > y2)
+            ky = -1;
+
+        // Time for fun trig
+        double theta = Math.atan(Math.abs(y2-y1)/Math.abs(x2-x1));
+
+        float realX1 = x1+kx*r1*(float)Math.cos(theta);
+        float realY1 = y1+ky*r1*(float)Math.sin(theta);
+
+        float realX2 = x2-kx*r2*(float)Math.cos(theta);
+        float realY2 = y2-ky*r2*(float)Math.sin(theta);
+
+        Paint connectionPaint = new Paint();
+        connectionPaint.setColor(Color.rgb(0,49,94));
+        connectionPaint.setStrokeWidth(5.0f);
+        canvas.drawLine(realX1,realY1,realX2,realY2,connectionPaint);
+
+        /*Paint dotPaint = new Paint();
+        dotPaint.setColor(Color.RED);
+        canvas.drawCircle(realX1,realY1,4,dotPaint);
+        canvas.drawCircle(realX2,realY2,4,dotPaint);*/
+
+
+
+    }
+
     public boolean onTouchEvent(MotionEvent ev)
     {
         gestureController.onTouchEvent(ev);
