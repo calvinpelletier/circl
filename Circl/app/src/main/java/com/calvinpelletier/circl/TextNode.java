646a775779b823e794d46a156c2e99304c8fe0e3
Wilson Chaney <wilsongchaney@gmail.com>
2015-10-17 20:24:18 -0500

    TextNode constructor

--- Circl/app/src/main/java/com/calvinpelletier/circl/TextNode.java
+++ Circl/app/src/main/java/com/calvinpelletier/circl/TextNode.java
@@ -1,17 +1,19 @@
 package com.calvinpelletier.circl;
 
-import org.w3c.dom.Text;
-
 /**
  * Created by cpelletier on 9/26/15.
  */
+
+// Object for any node with *text* associated with it
 public class TextNode extends Node {
 
     public String title = "";
     public String content = "";
 
-    public TextNode(Coord position, int outlineColor) {
+    public TextNode(Coord position, int outlineColor, String title, String content) {
         super(position, outlineColor);
+        this.title = title;
+        this.content = content;
     }
 
 }
